42de65ed3edf2177f99bdd38ddf6d9aa3a77a4d7
Jozadaque Batista <jozadaquebatista@gmail.com>
2016-09-24 17:16:26 -0300

    Update count_logs.sh

--- count_logs.sh
+++ count_logs.sh
@@ -9,19 +9,24 @@
  */
  
 'uname' -a;
+echo -e "\e[97m";
 echo -e "            __";
 echo -e ".+=========|==|===========+.";
 echo -e "|  WELCOME TO \e[7mTERM\e[27mCLEANER  |";
 echo -e "'+=========|==|===========+'";
 echo -e "'----------|  |------------'";
 echo -e "           '--'";
-printf "\n@author: \e[4mjozadaquebatista";
+echo -e "\e[92m";
+printf "\n@author: \e[4m\e[1mjozadaquebatista";
 
 # CROSS PLATFORM
     echo -e 'CLEANING CACHE ...\n'     ;
     sudo rm -Rf ~/.cache              ;
     echo -e 'CLEANING LOGS ...\n'      ;
-    sudo find / -name *.log -print0 -delete | wc -l;
+    sudo find / -name *.log -print0 -delete;
+    sudo find / -name *.DS_Store -print0 -delete;
+    sudo find / -name Thumbs.db -print0 -delete;
+    sudo rm -Rf /tmp/*
  
 if[ uname -eq 'Linux' ]; then
     sudo rm -Rf /var/cache/*
